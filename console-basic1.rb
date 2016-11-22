c1e16716eb75c5dd9c3f3c98562b2bd563dba45d
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2016-11-22 19:20:49 +0600

    user colorize gem for coloring and user Readline for getting screen size

--- console-basic1.rb
+++ console-basic1.rb
@@ -2,15 +2,21 @@
 #	KING	SABRI	|	@KINGSABRI
 #	
 require	'readline'
+require 'colorize'
+require	'readline'
+
 #	Prevent	Ctrl+C	for	exiting
 trap('INT',	'SIG_IGN')
 #	List	of	commands
 CMDS	=	[	'help',	'rubyfu',	'ls',	'pwd',	'exit'	].sort
-completion	=	proc	{	|line|	CMDS.grep(	/^#{Regexp.escape(line)}/	)	}
+completion	=	proc	{	|line|	CMDS.grep(/^#{Regexp.escape(line)}/)	}
+
 #	Console	Settings
 Readline.completion_proc	=	completion								#	Set	completion	process
 Readline.completion_append_character	=	'	'			#	Make	sure	to	add	a	space	after	completion
+
 while	line	=	Readline.readline('->	',	true)
-	puts	line	unless	line.nil?	or	line.squeeze.empty?
+	Readline.get_screen_size.last.times { print "-".blue }
+	puts	line.green	unless	line.nil?	or	line.squeeze.empty?
 	break	if	line	=~	/^quit.*/i	or	line	=~	/^exit.*/i
 end
